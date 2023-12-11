88112e05d827974d31344d871dc13e4b7e76e5ed
user <user@DESKTOP-COJ9858>
2023-12-11 12:12:23 +0900

    아이콘 추가 + 강좌 목록 보기

--- CultureCenter/src/main/java/com/culture/demo/mapper/AppSearchMapper.java
+++ CultureCenter/src/main/java/com/culture/demo/mapper/AppSearchMapper.java
@@ -11,11 +11,11 @@ public interface AppSearchMapper {
 	
 	// 강좌 정보 가져오기
 	List<ClassDTO> selectClassList(@Param("branch_id") int branch_id, @Param("searchBranchDTO") SearchBranchDTO searchBranchDTO,
-		    @Param("yyl") String[] yyl,
-		    @Param("lectcll") String[] lectcll,
-		    @Param("lectstl") String[] lectstl,
-		    @Param("dayl") String[] dayl,
-		    @Param("timel") String[] timel,
-		    @Param("amtl") String[] amtl) throws Exception;
+								    @Param("yyl") String[] yyl,
+								    @Param("lectcll") String[] lectcll,
+								    @Param("lectstl") String[] lectstl,
+								    @Param("dayl") String[] dayl,
+								    @Param("timel") String[] timel,
+								    @Param("amtl") String[] amtl) throws Exception;
 
 }
