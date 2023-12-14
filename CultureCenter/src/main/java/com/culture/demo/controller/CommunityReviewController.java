43b3e0fb530e55b3719f9038f1b42e2cab230bf4
Junhee27 <leejunhee0207@naver.com>
2023-12-14 16:35:55 +0900

    리뷰 확인

--- CultureCenter/src/main/java/com/culture/demo/controller/CommunityReviewController.java
+++ CultureCenter/src/main/java/com/culture/demo/controller/CommunityReviewController.java
@@ -34,6 +34,7 @@ public class CommunityReviewController {
 	private LecSearchService lecSearchService;
 	
 	
+	
 	// 리뷰 페이지
 	@GetMapping("list.do")
 	public String getReviewList(FrmSearchDTO frmSearchDTO, Model model) throws ClassNotFoundException, SQLException {
