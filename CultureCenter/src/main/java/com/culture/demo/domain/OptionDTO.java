6476fe86420aac02fecd286eb59c5feadb716636
GYEONGSEO LEE <lgs9255@gmail.com>
2023-12-18 10:15:20 +0900

    장바구니 버튼 구현, 강좌 상세보기 페이지 내 옵션 선택 구현

--- CultureCenter/src/main/java/com/culture/demo/domain/OptionDTO.java
+++ CultureCenter/src/main/java/com/culture/demo/domain/OptionDTO.java
@@ -9,7 +9,27 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class OptionDTO {
 	
+	private int class_id;
 	private int detail_class_sq;
 	private int ex_charge;
+	private String mon;
+	private String tue;
+	private String wed;
+	private String thu;
+	private String fri;
+	private String sat;
+	private String sun;
+	private String start_time;	
+	private String end_time;	
+	private int class_cnt;
+	private String schedule_start_dt;	
+	private String schedule_end_dt;
+	private String reception_start_dt;	
+	private String reception_end_dt;		
+	
+	private String day;
+	private String classDuration;
+	private String classTime;
+	private String recepDuration;
 	
 }
